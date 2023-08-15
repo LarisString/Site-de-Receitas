63b911d53f0fa5c3112f8cecf1183985c14550e4
LarisString <larissastring@gmail.com>
2023-08-15 12:04:31 -0300

    : trying new js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var controleCampo = 1;
 function adicionarCampo() {
     controleCampo++;
    
-    document.getElementById ('Ingredientes').insertAdjacentHTML('beforeend', '<div class="form-receita" id="campo' + controleCampo + '"><label>Ingredientes: </label><input type="text" name="Ingredientes[]" id="Ingredientes"></div>')
+    document.getElementById ('Ingredientes').insertAdjacentHTML('beforeend', '<div class="form-receita" id="campo' + controleCampo + '"><label>Ingredientes: </label><input type="text" name="Ingredientes[]' + controleCampo + '" id="Ingredientes' + controleCampo + '"><button type="submit" onclick="adicionarCampo()"> + </button><br><br></div>')
 
 }
