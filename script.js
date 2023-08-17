edf1df50e782a92d6ddeb131e96144d6696833ba
LarisString <larissastring@gmail.com>
2023-08-17 12:38:17 -0300

    : starting css

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var controleCampo = 1;
 function adicionarCampo() {
     controleCampo++;
    
-    document.getElementById ('Ingredientes').insertAdjacentHTML('beforeend', '<div class="form-receita" id="campo' + controleCampo + '"><label>Ingrediente: </label><input type="text" name="Ingredientes[]' + controleCampo + '" id="Ingredientes' + controleCampo + '"><button type="submit" onclick="adicionarCampo()"> + </button><br><br></div>')
+    document.getElementById ('Ingredientes').insertAdjacentHTML('beforeend', '<div class="form-receita" id="campo' + controleCampo + '"><label>Ingrediente: </label><input type="text" name="Ingrediente[]' + controleCampo + '" id="Ingrediente' + controleCampo + '"><button type="submit" onclick="adicionarCampo()"> + </button><br><br></div>')
 
 }
